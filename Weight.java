03596abd947c637603bef53199fd3a1d589357ed
Noga Wallach <noga.wallach@gmail.com>
2026-09-23 12:01:06 -0700

    weight 7 ooo llaaa

--- Weight.java
+++ Weight.java
@@ -1,9 +1,43 @@
-public class Weight{
-    
+public class Weight {
+
     private int pounds;
     private int ounces;
 
-    public void Weight(int b){
-        if(0<b<15)
+    public Weight(int ounces) {
+        this(ounces, 0);
+    }
+
+    public Weight(int ounces, int pounds) {
+        if (ounces >= 0) {
+            if (ounces < 16) {
+                this.ounces = ounces;
+            }
+        }
+
+        if (pounds > 0) {
+            this.pounds = pounds;
+        }
+    }
+
+    public int totalOunces(){
+        return this.ounces + 16 * this.pounds;
+    }
+
+    public boolean isHeavier(Weight other){
+        int totalW = this.totalOunces();
+        int otherW = other.totalOunces();
+        return totalW > otherW;
+        // if(totalW > otherW)
+        //     return true;
+        // else
+        //     return false; 
+    }
+
+    public Weight multiple(int scale){
+        int total = totalOunces() * scale;
+        int totalP = total / 16;
+        int totalO = totalOunces() - (totalP * 16);
+        return new Weight(totalP, totalO);
+        
     }
 }
