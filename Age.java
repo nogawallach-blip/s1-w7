a41f8b6a8119119a4d0f60136d84c73e46ed3012
Noga Wallach <noga.wallach@gmail.com>
2026-09-25 12:21:02 -0700

    wee 7 pt 2

--- Age.java
+++ Age.java
@@ -3,7 +3,7 @@ public class Age{
     //No Constructor -- do you remember what this mean?
     
     public boolean isTeenAger(int age){
-        //remove this when you start
-        return false;
+         return 13 <= age && age <= 19;0
+
     }
 }
--- AgeMain.java
+++ AgeMain.java
@@ -1,6 +1,8 @@
 public class AgeMain{
     public static void main(String[] args) {
-        
+        Age a = new Age();
+        boolean one = a.isTeenAger(19);
+        System.out.println(one);
     }
 
 }
